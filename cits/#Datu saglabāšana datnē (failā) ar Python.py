7ca69990e78aa9b51b09566c0f252d8130e1c6d7
darkakarzinina <darja.karzinina@gmail.com>
2026-05-07 20:15:34 +0300

    update

---
+++ cits/#Datu saglabāšana datnē (failā) ar Python.py
@@ -0,0 +1,11 @@
+#Datu saglabāšana datnē (failā) ar Python
+#07/05/26
+
+#open() - atver vai izveido failu
+#encoding="utf-8" - ļauj saglabāt latviešu burtus
+#f.write() - ieraksta tekstu failā
+#"w" - izveido / pārraksta failu (dzēš iepriekšējo saturu.)
+#"r" - lasa failu
+#"a" - papildina failu (pievieno datus beigās)
+#\n - nozīmē jauna rinda
+
